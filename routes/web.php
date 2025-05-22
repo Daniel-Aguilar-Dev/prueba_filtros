0b7f67474f764b76bc41960cd8ee15aa5657e60f
Juan-Esq-dv <20233tn066@utez.edu.mx>
2025-05-22 13:22:18 -0600

    webos

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,26 @@
-<?php
+    <?php
 
-use App\Http\Controllers\sistema\general\UserController;
-use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\sistema\general\PermisoController;
+    use App\Http\Controllers\sistema\general\UserController;
+    use Illuminate\Support\Facades\Route;
+    use Spatie\Permission\Contracts\Permission;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('/', function () {
+        return view('welcome');
+    });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+    Route::middleware([
+        'auth:sanctum',
+        config('jetstream.auth_session'),
+        'verified',
+    ])->group(function () {
+        Route::get('/dashboard', function () {
+            return view('dashboard');
+        })->name('dashboard');
+    });
 
 
-Route::resource('user', UserController::class)->names('user');
-Route::get('user/data', [UserController::class, 'data'])->name('user.data');
+    Route::resource('user', UserController::class)->names('user');
+    Route::get('user/data', [UserController::class, 'data'])->name('user.data');
+
+    Route::get('permisos', [PermisoController::class, 'index'])->name('permisos.index');
